e069d430f9dd026669fb606e2824992fd0ee473a
Ayaaz Qureshi <ayaaz.qureshi@gmail.com>
2023-11-15 21:40:35 -0500

    Improvements

--- qanda.py
+++ qanda.py
@@ -1,15 +1,33 @@
 import os
 import openai
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt="I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:",
-    temperature=0,
-    max_tokens=100,
-    top_p=1,
-    frequency_penalty=0.0,
-    presence_penalty=0.0,
-    stop=["\n"]
-)
+def get_response(question: str):
+    try:
+        return openai.Completion.create(model="text-davinci-003", prompt=question, temperature=0, max_tokens=100,
+                                        top_p=1, frequency_penalty=0.0, presence_penalty=0.0, stop=["\n"])
+    except Exception as err:
+        print(err)
+
+
+def interactive_mode():
+    intro_message = "I am a highly intelligent question answering bot. If you ask me a question that is rooted in "
+    intro_message += "truth, I will give you the answer.\n"
+    intro_message += "If you ask me a question that is nonsense, trickery, or has no clear answer, "
+    intro_message += "I will respond with \"Unknown\""
+    prompt = "What's your question? [type \"stop\"] to end: "
+    print(intro_message)
+    while True:
+        question = input(prompt)
+        if question == "stop":
+            print("Goodbye")
+            break
+        response = get_response(question)
+        print(response)
+
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+if __name__ == "__main__":
+    interactive_mode()
+else:
+    print("to-do: library")
